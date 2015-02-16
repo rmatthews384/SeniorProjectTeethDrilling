a33100caf0e7eea372413280ec4cfad19a2cc01e
rmatthews384 <rmatthews384@yahoo.com>
2015-02-16 18:44:02 -0500

    Adding initial work

---
+++ Assets/CubeController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeController : MonoBehaviour {
+
+	public Material[] materials;
+	int decayvalue;
+
+	void Awake ()
+	{
+		decayvalue = 0;
+	}
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	public int getDecay()
+	{
+		return this.decayvalue;
+	}
+
+	public void setMaterial(int material)
+	{
+		gameObject.renderer.material = materials [material];
+		Debug.Log ("setMaterial");
+		decayvalue = material;
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
