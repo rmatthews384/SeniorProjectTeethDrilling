984a73dd6854753ef103f75b54fd1a94f3cfc0df
rmatthews384 <rmatthews384@yahoo.com>
2015-04-04 10:46:04 -0400

    ...

--- Assets/Scripts/Ones I'm assuming currently/cubeTimer.cs
+++ Assets/Scripts/Ones I'm assuming currently/cubeTimer.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class cubeTimer : MonoBehaviour {
-
+	
 	float timer;
 	bool hover;
 	PoolingSystem pS;
@@ -10,6 +10,7 @@ public class cubeTimer : MonoBehaviour {
 	Global global;
 	static float cubeDim = 0.019f;
 
+	
 	// Use this for initialization
 	void Start () 
 	{
@@ -20,57 +21,59 @@ public class cubeTimer : MonoBehaviour {
 		timer = 0;
 		hover = false;
 	}
-
-	void OnMouseDown()
-	{
-		global.setMouseDown (true);
-
-	}
-
+	
 	void OnMouseEnter()
 	{
 		hover = true;
 	}
-
+	
 	void OnMouseExit()
 	{
 		hover = false;
 	}
-
+	
 	// Update is called once per frame
 	void Update () 
 	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			global.setMouseDown(true);
+		}
 		if (Input.GetMouseButtonUp (0))
 		{
 			global.setMouseDown(false);
 		}
-
+		
 		if(global.getMouseDown() && hover)
 		{
 			timer += Time.deltaTime;
 		}
 		if(timer >= .1f)
 		{
-			timer = 0;
-			Vector3 myposition = this.gameObject.transform.position;
-			Vector3 myoldposition;
-			myposition.z += cubeDim;
-			pS.InstantiateAPS("cube", myposition, Quaternion.identity);
-			myoldposition = myposition;
-			myposition.x += cubeDim;
-			pS.InstantiateAPS("cube", myposition, Quaternion.identity);
-			myposition = myoldposition;
-			myposition.x -= cubeDim;
-			pS.InstantiateAPS("cube", myposition, Quaternion.identity);
-			myposition = myoldposition;
-			myposition.y += cubeDim;
-			pS.InstantiateAPS("cube", myposition, Quaternion.identity);
-			myposition = myoldposition;
-			myposition.y -= cubeDim;
-			pS.InstantiateAPS("cube", myposition, Quaternion.identity);
+			if(cc.getLevel() < 20)
+			{
+				timer = 0;
+				Vector3 myposition = this.gameObject.transform.position;
+				Vector3 myoldposition;
+				myposition.z += cubeDim;
+				pS.InstantiateAPS("cube", myposition, Quaternion.identity);
+				myoldposition = myposition;
+				myposition.x += cubeDim;
+				pS.InstantiateAPS("cube", myposition, Quaternion.identity);
+				myposition = myoldposition;
+				myposition.x -= cubeDim;
+				pS.InstantiateAPS("cube", myposition, Quaternion.identity);
+				myposition = myoldposition;
+				myposition.y += cubeDim;
+				pS.InstantiateAPS("cube", myposition, Quaternion.identity);
+				myposition = myoldposition;
+				myposition.y -= cubeDim;
+				pS.InstantiateAPS("cube", myposition, Quaternion.identity);
+			}
 			
 			PoolingSystemExtensions.DestroyAPS(this.gameObject);
-
+			
 		}
 	}
 }
+
